1c78d08bf9fdade8b8bc2081a3b2563ebfca4333
Jose Szychowski <joseszychowski@gmail.com>
2022-09-14 10:28:02 -0300

    Modify home for test

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 export function loadHomePage(){
     const homePage = document.getElementById("custom-page")
     homePage.innerHTML = (`<div class="home-container">
-    <p>A restaurant is a business that prepares and serves food and drinks to customers.[1] Meals are generally served and eaten on the premises, but many restaurants also offer take-out and food delivery services. Restaurants vary greatly in appearance and offerings, including a wide variety of cuisines and service models ranging from inexpensive fast-food restaurants and cafeterias to mid-priced family restaurants, to high-priced luxury establishments.</p>
+    <p>A restaurante is a business that prepares and serves food and drinks to customers.[1] Meals are generally served and eaten on the premises, but many restaurants also offer take-out and food delivery services. Restaurants vary greatly in appearance and offerings, including a wide variety of cuisines and service models ranging from inexpensive fast-food restaurants and cafeterias to mid-priced family restaurants, to high-priced luxury establishments.</p>
     </div>`);
     
 }
